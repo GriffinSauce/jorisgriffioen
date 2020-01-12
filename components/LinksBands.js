60f5d3301be1affa084c669abcd60f7c764d88dc
GriffinSauce <jorisjgriffioen@gmail.com>
2020-01-12 23:16:52 +0100

    Make band links a bit less crazy

--- components/LinksBands.js
+++ components/LinksBands.js
@@ -6,15 +6,11 @@ export default () => (
     <nav>
       <a className="cs" target="_blank" href="https://www.coralsprings.nl/">
         <img alt="Coral Springs" src="logo-coralsprings.svg" />
-        <div className="description">
-          <span>Coral Springs - pop punk</span>
-        </div>
+        <div className="description">Coral Springs - pop punk</div>
       </a>
       <a className="la" target="_blank" href="https://www.leftalive.nl/">
         <img alt="Left Alive" src="logo-leftalive.svg" />
-        <div className="description">
-          <span>Left Alive - pop punk</span>
-        </div>
+        <div className="description">Left Alive - pop punk</div>
       </a>
     </nav>
     <style jsx>{`
@@ -27,16 +23,34 @@ export default () => (
 
       a {
         display: block;
+        position: relative;
         margin: 0 0 20px 0;
-        padding: 20px 0;
+        padding: 18px 0;
         flex-basis: 100%;
-        background-color: rgba(255, 255, 255, 0.05);
         border: 1px solid ${theme.color4};
+      }
+
+      a:before {
+        display: block;
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.3);
+        z-index: 1;
         transition: box-shadow 200ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
       }
 
-      a:hover {
+      a:hover:before {
         box-shadow: inset 0 0 0 4px ${theme.color4};
+      }
+
+      a img,
+      a .description {
+        position: relative;
+        z-index: 2;
       }
 
       a img {
@@ -54,38 +68,27 @@ export default () => (
         background-size: cover;
       }
       a.cs {
+        background-position: top;
         background-image: url('/vid-cs.webp');
       }
       a.la {
+        background-position: top;
         background-image: url('/vid-la.webp');
       }
 
       .description {
         position: relative;
         display: inline-block;
-        margin-top: 10px;
-        font-style: italic;
-      }
-
-      .description::before {
-        position: absolute;
-        z-index: 0;
-        display: block;
-        top: -6px;
-        left: -10px;
-        right: -10px;
-        bottom: -6px;
+        margin-top: 8px;
+        font-size: 1.05rem;
+        padding: 6px 10px;
         content: '';
-        transform: skewX(-8deg);
         background: linear-gradient(
           135deg,
           ${darken(0.35, theme.color5)},
           ${darken(0.4, theme.color1)}
         );
-      }
-      .description span {
-        position: relative;
-        z-index: 1;
+        transform: skewX(-8deg);
       }
 
       @media (min-width: 992px) {
