19adca922579a8fb62839658c2c00c110fff7292
Joris Griffioen <jorisjgriffioen@gmail.com>
2020-08-31 21:28:54 +0200

    Preload fonts

--- components/Layout.js
+++ components/Layout.js
@@ -34,8 +34,10 @@ export default ({ children }) => (
 
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+      <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+      <link rel="preload" href="https://fonts.googleapis.com/css?family=Montserrat:400,600|Oswald:300,400&display=swap" as="style" />
       <link
-        href="https://fonts.googleapis.com/css?family=Montserrat:400,600|Oswald:300,400"
+        href="https://fonts.googleapis.com/css?family=Montserrat:400,600|Oswald:300,400&display=swap"
         rel="stylesheet"
       />
 
