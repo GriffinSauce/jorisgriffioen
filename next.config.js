cc0d7aba64d524057934807bdd85ff1500830267
GriffinSauce <jorisjgriffioen@gmail.com>
2020-11-01 21:21:20 +0100

    Set basic i18n setting to improve a11y

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withPreact = require('next-plugin-preact');
 
-module.exports = withPreact();
+module.exports = withPreact({
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+});
