10a913512b87a7695643cedcfc9d4c84e8c88c90
Joris Griffioen <jorisjgriffioen@gmail.com>
2021-05-26 22:16:56 +0200

    Chromebook dev proof of concept

---
+++ pages/pookie.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Layout from '../components/Layout';
+import Logo from '../components/Logo';
+import Links from '../components/Links';
+import LinksBands from '../components/LinksBands';
+
+const Pookie = () => (
+  <Layout>
+    <h1>Hello pookie!</h1>
+    <p>Made this on the chromey :D</p>
+  </Layout>
+);
+
+export default Pookie;
